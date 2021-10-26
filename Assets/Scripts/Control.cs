db6413f68ef4c252239db6c0cf914318c0036676
Victor <Legendary.nr.23@gmail.com>
2021-10-26 17:01:54 +0200

    fed

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -8,20 +8,23 @@ public class Control : MonoBehaviour
 
     private Rigidbody rb;
 
-    public bool moving;
-    //public bool SpeedBoost;
+    public float dashspeed;
 
+    public float CooldownTime = 5;
+
+    public float NextFireTime = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        //InvokeRepeating("CheckDrag", 0.1f, 0.2f);
+
+
     }
 
-   
-    
+
+
     // Update is called once per frame
     void Update()
     {
@@ -32,17 +35,6 @@ public class Control : MonoBehaviour
     {
         if (gameObject.CompareTag("Player1"))
         {
-           /* if (Input.GetKeyDown("space"))
-            if (speed == 10)
-            {
-                speed = 15;
-            }
-            else
-                {
-                    speed = 10;
-                }
-           */
-
 
             float moveHorizontal = Input.GetAxis("Horizontal1");
             //float Jump = Input.GetAxis("Jump");
@@ -50,47 +42,53 @@ public class Control : MonoBehaviour
 
             Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
+            rb.AddForce(movement * speed, ForceMode.Impulse);
 
-            //rb.transform.Translate(movement * speed);  //God Speed = 0.05
-            rb.AddForce(movement * speed, ForceMode.Impulse); //God Speed = 10
-            //rb.velocity = movement * speed;
-            
+            if (Time.time > NextFireTime)
+            {
+                if (Input.GetKey("l"))
+                {
+                    rb.AddForce(movement * dashspeed, ForceMode.Impulse);
+                    NextFireTime = Time.time + CooldownTime;
 
+                }
+            }
         }
 
 
         else
         {
-           
-            /*if (Input.GetKeyDown("f"))
-            if (rb.mass == 1)
-            {
-                    rb.mass = 10;
-            }
-            else
-                {
-                    rb.mass = 1;
-                }
-            */
-            
+
             float moveHorizontal = Input.GetAxis("Horizontal2");
-            float moveVertical = Input.GetAxis("Vertical2");        
+            float moveVertical = Input.GetAxis("Vertical2");
             Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-            
-            //rb.transform.Translate(movement * speed);  //God Speed = 0.05
-            rb.AddForce(movement * speed, ForceMode.Impulse); //God Speed = 10
-            //rb.velocity = (movement * speed);
-          
+
+
+            rb.AddForce(movement * speed, ForceMode.Impulse);
+
+            if (Time.time > NextFireTime)
+            {
+                if (Input.GetKey(KeyCode.Space))
+                {
+
+                    rb.AddForce(movement * dashspeed, ForceMode.Impulse);
+                    NextFireTime = Time.time + CooldownTime;
+
+                }
+            }
         }
-        
-        
+
+
     }
     private void OnCollisionEnter(Collision collision)
     {
-        //rb.drag = 1;
+
     }
 
-    private void OnTriggerEnter(Collider other)
+
+
+
+   /* private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PowerUP"))
         {
@@ -99,5 +97,5 @@ public class Control : MonoBehaviour
         // Indtil videre er der ingen reel PowerUP
         Debug.Log("TORNADO");
 
-    }
+    }*/
 }
