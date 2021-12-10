11be288985e66961fd2a87a45eecea440f3b1a46
Victor <Legendary.nr.23@gmail.com>
2021-12-10 13:16:10 +0100

    Tutorial popups 1+2

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
+    public GameObject[] popUps;
+    private int popUpIndex;
+    public ResetTrigger[] SkyRespawnList;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,44 @@ public class Tutorial : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if(i == popUpIndex)
+            {
+                popUps[i].SetActive(true);
+            }
+            else
+            {
+                popUps[i].SetActive(false);
+            }
+        }
+        if(popUpIndex == 0)
+        {
+            GameObject.Find("Player1").GetComponent<Control>().enabled = false;
+            GameObject.Find("Player2").GetComponent<Control>().enabled = false;
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 1)
+        {
+            foreach (ResetTrigger script in SkyRespawnList)
+            {
+                script.GetComponent<ResetTrigger>().enabled = false;
+                script.GetComponentInChildren<ParticleSystem>().Stop();
+            }
+            GameObject.Find("Player1").GetComponent<Control>().enabled = true;
+            GameObject.Find("Player2").GetComponent<Control>().enabled = true;
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 2)
+        {
+
+        }
     }
 }
